b283bddf32223c210a6279d5a9c6b8ebfe1f15aa
arjunthazhath2001 <arjunta32@gmail.com>
2025-07-31 08:00:02 +0530

    ok

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI,status, HTTPException,Depends
+from fastapi import FastAPI,status, HTTPException,Depends,Response
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -23,8 +23,6 @@ class Post(BaseModel):
     title:str
     content: str
     published:bool=True
-    rating: Optional[int]= None
-
 
 while True:
     try:
@@ -68,49 +66,63 @@ async def latest_post():
 
 #get specific post
 @app.get('/posts/{id}')
-async def get_post(id:int):  
+async def get_post(id:int,db:Session=Depends(get_db)):  
     try:
-        cursor.execute("SELECT * FROM posts WHERE id=%s",(str(id),))
-        post=cursor.fetchone()
+        # cursor.execute("SELECT * FROM posts WHERE id=%s",(str(id),))
+        # post=cursor.fetchone()
+        post=db.query(models.Post).filter(models.Post.id==id).first()
         return {"result":post}
     except: 
         HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="not found")
   
 #create a post
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
-async def create_posts(post:Post):
+async def create_posts(post:Post,db: Session=Depends(get_db)):
     try:
-        cursor.execute("INSERT INTO posts(title,content,published) VALUES(%s,%s,%s)RETURNING *",(post.title,post.content,post.published))
-        new_post=cursor.fetchone()
-        conn.commit()
+        # cursor.execute("INSERT INTO posts(title,content,published) VALUES(%s,%s,%s)RETURNING *",(post.title,post.content,post.published))
+        # new_post=cursor.fetchone()
+        # conn.commit()
+
+        new_post=models.Post(**post.dict())
+        db.add(new_post)
+        db.commit()
+        db.refresh(new_post)
         return {"message":f"post created: {new_post}"}
-    except:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="couldn't create")
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"couldn't create: {str(e)}")
+
 
 
 
 #delete a post
 @app.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id:int):
+async def delete_post(id:int,db:Session=Depends(get_db)):
 
-    cursor.execute("DELETE FROM posts where id=%s RETURNING *",(str(id),))
-    deleted_post= cursor.fetchone()
-    conn.commit()
+    # cursor.execute("DELETE FROM posts where id=%s RETURNING *",(str(id),))
+    # deleted_post= cursor.fetchone()
+    # conn.commit()
+    post=db.query(models.Post).filter(models.Post.id==id)
 
-    if deleted_post==None:
+    if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} does not exist")
     else:
-        return {"deleted":deleted_post}  
-
+        post.delete(synchronize_session=False)
+        db.commit()
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
 
 @app.patch('/posts/{id}',status_code=status.HTTP_200_OK)
-async def update_post(id:int,p:Post):
-    cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *",(p.title,p.content,p.published,str(id)))
-    updated_post=cursor.fetchone()
+async def update_post(id:int,updated_post:Post,db:Session=Depends(get_db)):
+    # cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *",(p.title,p.content,p.published,str(id)))
+    # updated_post=cursor.fetchone()
 
-    conn.commit()
-    
-    if updated_post==None:
+    # conn.commit()
+    post= db.query(models.Post).filter(models.Post.id==id)
+    if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} does not exist")
-
-    return {"updated post":updated_post}
+    else:
+        new_post=post.update(updated_post.dict(),synchronize_session=False)
+        db.commit()
+    return {"updated post":new_post}
