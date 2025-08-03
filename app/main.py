2a2252f3f25ae2946574c20f3eed8c6a1d2b06fc
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-03 15:03:27 +0530

    req res pydantic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from typing import Optional
 from fastapi import FastAPI,status, HTTPException,Depends,Response
-from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models
+from . import models,schemas
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 
@@ -13,16 +12,10 @@ from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 
-
 app= FastAPI()
 
 
 
-
-class Post(BaseModel):
-    title:str
-    content: str
-    published:bool=True
 
 while True:
     try:
@@ -42,11 +35,6 @@ def root():
     return {"message":"hi world"}
 
 
-@app.get("/sql")
-def testing(db: Session=Depends(get_db)):
-        print(posts)
-        return {"message":"success"}
-
 
 #get all posts
 @app.get('/posts')
@@ -54,7 +42,7 @@ async def get_posts(db: Session=Depends(get_db)):
     # cursor.execute("SELECT * FROM posts")
     # posts=cursor.fetchall()
     posts= db.query(models.Post).all()
-    return {'posts':posts}
+    return posts
 
 
 #get latest post
@@ -71,13 +59,13 @@ async def get_post(id:int,db:Session=Depends(get_db)):
         # cursor.execute("SELECT * FROM posts WHERE id=%s",(str(id),))
         # post=cursor.fetchone()
         post=db.query(models.Post).filter(models.Post.id==id).first()
-        return {"result":post}
+        return post
     except: 
         HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="not found")
   
 #create a post
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
-async def create_posts(post:Post,db: Session=Depends(get_db)):
+async def create_posts(post:schemas.PostCreate,db: Session=Depends(get_db)):
     try:
         # cursor.execute("INSERT INTO posts(title,content,published) VALUES(%s,%s,%s)RETURNING *",(post.title,post.content,post.published))
         # new_post=cursor.fetchone()
@@ -87,7 +75,7 @@ async def create_posts(post:Post,db: Session=Depends(get_db)):
         db.add(new_post)
         db.commit()
         db.refresh(new_post)
-        return {"message":f"post created: {new_post}"}
+        return new_post
     except Exception as e:
         import traceback
         traceback.print_exc()
@@ -114,7 +102,7 @@ async def delete_post(id:int,db:Session=Depends(get_db)):
     
 
 @app.patch('/posts/{id}',status_code=status.HTTP_200_OK)
-async def update_post(id:int,updated_post:Post,db:Session=Depends(get_db)):
+async def update_post(id:int,updated_post:schemas.PostCreate,db:Session=Depends(get_db)):
     # cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *",(p.title,p.content,p.published,str(id)))
     # updated_post=cursor.fetchone()
 
@@ -125,4 +113,4 @@ async def update_post(id:int,updated_post:Post,db:Session=Depends(get_db)):
     else:
         new_post=post.update(updated_post.dict(),synchronize_session=False)
         db.commit()
-    return {"updated post":new_post}
+    return new_post
