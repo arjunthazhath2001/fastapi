38a38f8f0e6b58c325a49d06cc85aa1b17a4abcc
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-05 08:28:07 +0530

    k

--- app/routers/auth..py
+++ app/routers/auth..py
@@ -14,4 +14,7 @@ def login(user_credentials:schemas.UserLogin,db: Session= Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
     
     if not utils.verify(user_credentials.password,user.password):
-        raise HTTPException(status_code=)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
+    
+
+    return {"token":"sample"}
