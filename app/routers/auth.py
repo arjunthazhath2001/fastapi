5fa1fe8c41784ea92efa154444696b1ffa6d23d2
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-06 07:10:28 +0530

    er

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter,Depends,status,HTTPException,Response
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import schemas,models,utils
+from .. import schemas,models,utils,oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router=APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(user_credentials:schemas.UserLogin,db: Session= Depends(get_db)):
-    user= db.query(models.User).filter(models.User.email==user_credentials.email).first()
+def login(user_credentials:OAuth2PasswordRequestForm=Depends(),db: Session= Depends(get_db)):
+    
+    #oauth only returns username and pswd----not email----since u r using OAuthForm, send request in postman using formdata not raw-data
+    
+    user= db.query(models.User).filter(models.User.email==user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
@@ -16,5 +20,6 @@ def login(user_credentials:schemas.UserLogin,db: Session= Depends(get_db)):
     if not utils.verify(user_credentials.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credentials")
     
+    access_token=oauth2.create_access_token(data={"user_id":user.id})
 
-    return {"token":"sample"}
+    return {"access_token":access_token,"token_type":"bearer"}
