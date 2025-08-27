2cb35e58f52653ef811340d7220c6cc89203bdf4
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-27 22:40:17 +0530

    ok

--- app/models.py
+++ app/models.py
@@ -20,7 +20,3 @@ class User(Base):
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
 
 
-
-# class Trial():
-#     def test():
-#         return "it works"
