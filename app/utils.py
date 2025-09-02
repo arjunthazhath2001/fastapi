8f533d804052b7379e898fe81e5c0bfde0e4a31d
arjunthazhath2001 <arjunta32@gmail.com>
2025-09-02 23:58:02 +0530

    ok

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,8 @@ pwd_context= CryptContext(schemes=["bcrypt"], deprecated="auto")
 #sepcial purpose functions
 
 def hash(password:str):
-    return pwd_context.hash(password)
+    return pwd_context.
+    hash(password)
 
 def verify(plain_password,hashed_password):
     return pwd_context.verify(plain_password,hashed_password)
